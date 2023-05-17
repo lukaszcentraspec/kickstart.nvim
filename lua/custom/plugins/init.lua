5d919ccbd62a75cfe8499cf4ea3f1cf0b21b3547
Lukasz Koczocik <lukasz@centraspec.com>
2023-05-17 12:11:07 +0100

    Konfiguracja cz.1

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,9 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+--return {
+--	require 'custom.plugins.remap',
+--	require 'custom.plugins.harpoon',
+--	require 'custom.plugins.colors'
+--}
 return {}
